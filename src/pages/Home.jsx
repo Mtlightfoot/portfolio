850aba0d7ca155db524cbf45b9b1e9ffe280845a
Matt Lightfoot <m-lightfoot@hotmail.com>
2024-07-09 17:00:34 +0100

    Finished front page, added links to socials, completed skills information

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 const styles = {
     leftColumn: {
@@ -17,6 +18,7 @@ const styles = {
     },
     profileInfo: {
         marginTop: '10px',
+        marginLeft: '6px'
     },
     title: {
         color: "#E3E0D3",
@@ -30,7 +32,6 @@ const styles = {
         fontWeight: "900",
         color: '#E3E0D3'
     },
-
     skills: {
         color: "#7D7C70",
         fontWeight: '600',
@@ -51,15 +52,27 @@ function Home() {
                                 Lightfoot
                             </h2>
                         </div>
+
                         <img id='profilePicture' src="./profile-picture.jpg" alt="A profile picture of Matt Lightfoot" />
-                        <div style={styles.profileInfo}></div>
-                        <h6 style={styles.title}><em>Specialisation:</em><br></br>
-                            <span style={styles.subtitle}>Front End Web Development</span></h6>
-                        <h6 style={styles.title}><em>Based in:</em><br></br>
-                            <span style={styles.subtitle}>Rugby, Warwickshire</span></h6>
+
+                        <div style={styles.profileInfo}>
+                            <h6 style={styles.title}><em>Specialisation:</em><br></br>
+                                <span style={styles.subtitle}>Front End Web Development</span></h6>
+                            <h6 style={styles.title}><em>Based in:</em><br></br>
+                                <span style={styles.subtitle}>Rugby, Warwickshire</span></h6>
+                        </div>
+
+                        <div id='iconRow'>
+                            <a href="mailto:m-lightfoot@hotmail.com"><img id='socialIcons' src="./email-icon.png" alt="An icon representing Email" /></a>
+                            <a href="tel:07801963340"><img id='socialIcons' src="./phone-icon.png" alt="An icon representing a phone" /></a>
+                            <a href="www.linkedin.com/in/matthew-lightfoot"><img id='socialIcons' src="./linkedin-icon.png" alt="An icon of the LinkedIn Logo" /></a>
+                            <a href="https://github.com/Mtlightfoot"><img id='socialIcons' src="./git-icon.png" alt="An icon of the Git Logo" /></a>
+                        </div>
+
                     </div>
                 </Col>
                 <Col md={8} id='rightColumn'>
+
                     <div style={styles.rightColumn} id='columnBox'>
                         <div>
                             <h1 style={styles.slogan} >I'm Matt Lightfoot
@@ -67,7 +80,11 @@ function Home() {
                                 Front End Web Developer.</h1>
                             <h1 style={styles.skills}>HTML - CSS - JavaScript - React.js - JQuery - Node.js - APIs - Bootstrap - Material UI</h1>
                         </div>
+                        <div>
+                            <a href='https://docs.google.com/document/d/1VZxcRjzsV2ZTtb0msm2PhW421wem9iupoWJ9Ka_OO4I/edit'><Button id='cvBtn'>Download CV</Button></a>
+                        </div>
                     </div>
+
                 </Col>
             </Row>
         </Container>
